a2a654954462d9a7e036dc193768e60be91a560a
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-04-14 06:43:41 +0000

    AutoPush

---
+++ push.sh
@@ -0,0 +1,7 @@
+git config --global user.email "seydiahmedelcheikh@gmail.com"
+git config --global user.name "mouhameddiouf"
+git config credential.helper store
+git rm -r --cached node_modules
+git add .
+git commit -m "AutoPush"
+git push
